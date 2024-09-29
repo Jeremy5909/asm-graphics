0f9727f2950086dc085737b31d233bd959af79de
Jeremy5909 <jeremyherczeg@gmail.com>
2024-09-28 23:55:13 -0700

    Draw Square

--- graphics.asm
+++ graphics.asm
@@ -11,15 +11,45 @@ start:
     mov al, 0x13
     int 0x10
 
-    mov edi, DRAW_START
-    mov byte [edi], COLOR_RED
+    call draw_square ; color 320x200
 
     jmp $
+
+draw_square:
+    mov edi, DRAW_START
+    mov eax, [sq_y]
+    mov ebx, 320
+    mul ebx
+    add eax, edi
+    mov edi, eax
+    add edi, [sq_x]
+
+    jmp .put_pixel
+
+.move_down:
+    add edi, 320
+    sub edi, [sq_width]
+    xor ecx, ecx
+.put_pixel:
+    mov byte [edi], COLOR_RED
+    inc edi
+    inc ecx
+    cmp ecx, [sq_width]
+    jl .put_pixel
+
+    inc edx
+    cmp edx, [sq_height]
+    jl .move_down
+.done:
+    ret
 
 
 begin_draw dd 0
 
-sq_width dd 40
+sq_x dd 100
+sq_y dd 50
+
+sq_width dd 70
 
 sq_height dd 70
 
